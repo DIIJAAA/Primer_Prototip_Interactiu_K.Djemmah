e54a8d60ad345e68cd0ca45f3d80f8a2b261d75d
DIIJAAA <khdj155@vidalibarraquer.net>
2025-03-07 20:01:16 +0100

    Acabat

---
+++ Primer_Prototip_Interactiu_K.Djemmah/Exemple_Control_SaveFileDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Primer_Prototip_Interactiu_K.Djemmah
+{
+    public partial class Exemple_Control_SaveFileDialog : Form
+    {
+        // Declarem els controls
+        private Button buttonObrirDialog;
+        private TextBox textBoxRutaFitxer;
+        private SaveFileDialog saveFileDialog;
+
+        public Exemple_Control_SaveFileDialog()
+        {
+            InitializeComponent();
+
+            // Configuració bàsica del formulari
+            this.Text = "Exemple de SaveFileDialog";
+            this.Size = new Size(400, 200);
+
+            // Creem i configurem el Button
+            buttonObrirDialog = new Button();
+            buttonObrirDialog.Location = new Point(50, 50);
+            buttonObrirDialog.Size = new Size(200, 30);
+            buttonObrirDialog.Text = "Obrir SaveFileDialog";
+            buttonObrirDialog.Click += ButtonObrirDialog_Click; // Assignem l'event
+
+            // Creem i configurem el TextBox
+            textBoxRutaFitxer = new TextBox();
+            textBoxRutaFitxer.Location = new Point(50, 100);
+            textBoxRutaFitxer.Size = new Size(200, 30);
+            textBoxRutaFitxer.ReadOnly = true; // El TextBox és de només lectura
+
+            // Creem i configurem el SaveFileDialog
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Fitxers de text (*.txt)|*.txt|Tots els fitxers (*.*)|*.*"; // Filtres per als tipus de fitxers
+            saveFileDialog.Title = "Desar fitxer"; // Títol del diàleg
+
+            // Afegim els controls al formulari
+            this.Controls.Add(buttonObrirDialog);
+            this.Controls.Add(textBoxRutaFitxer);
+        }
+
+        // Event que es dispara quan es fa clic al Button
+        private void ButtonObrirDialog_Click(object sender, EventArgs e)
+        {
+            // Obrim el SaveFileDialog
+            DialogResult resultat = saveFileDialog.ShowDialog();
+
+            // Si l'usuari fa clic a "Desar"
+            if (resultat == DialogResult.OK)
+            {
+                // Mostrem la ruta del fitxer seleccionat al TextBox
+                textBoxRutaFitxer.Text = saveFileDialog.FileName;
+            }
+        }
+    }
+}
---
+++ Primer_Prototip_Interactiu_K.Djemmah/Exemple_Control_TextBox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Primer_Prototip_Interactiu_K.Djemmah
+{
+    public partial class Exemple_Control_TextBox : Form
+    {
+        // Declarem els TextBoxs
+        private TextBox textBoxNormal;
+        private TextBox textBoxMaxLength;
+        private TextBox textBoxPassword;
+        private TextBox textBoxTextChanged;
+
+        public Exemple_Control_TextBox()
+        {
+            InitializeComponent();
+
+            // Configuració bàsica del formulari
+            this.Text = "Exemple de TextBox";
+            this.Size = new Size(400, 300);
+
+            // TextBox normal
+            textBoxNormal = new TextBox();
+            textBoxNormal.Location = new Point(50, 50);
+            textBoxNormal.Size = new Size(200, 30);
+            textBoxNormal.Text = "TextBox normal";
+
+            // TextBox amb MaxLength
+            textBoxMaxLength = new TextBox();
+            textBoxMaxLength.Location = new Point(50, 100);
+            textBoxMaxLength.Size = new Size(200, 30);
+            textBoxMaxLength.MaxLength = 10; // Limita a 10 caràcters
+            textBoxMaxLength.Text = "Màx 10 caràcters";
+
+            // TextBox amb PasswordChar
+            textBoxPassword = new TextBox();
+            textBoxPassword.Location = new Point(50, 150);
+            textBoxPassword.Size = new Size(200, 30);
+            textBoxPassword.PasswordChar = '*'; // Amaga el text amb asteriscs
+            textBoxPassword.Text = "contrasenya";
+
+            // TextBox amb l'event TextChanged
+            textBoxTextChanged = new TextBox();
+            textBoxTextChanged.Location = new Point(50, 200);
+            textBoxTextChanged.Size = new Size(200, 30);
+            textBoxTextChanged.TextChanged += TextBoxTextChanged_TextChanged; // Assignem l'event
+
+            // Afegim els TextBoxs al formulari
+            this.Controls.Add(textBoxNormal);
+            this.Controls.Add(textBoxMaxLength);
+            this.Controls.Add(textBoxPassword);
+            this.Controls.Add(textBoxTextChanged);
+        }
+
+        // Event que es dispara quan el text del TextBoxTextChanged canvia
+        private void TextBoxTextChanged_TextChanged(object sender, EventArgs e)
+        {
+            // Mostrem el text actual en el títol del formulari
+            this.Text = "Text canviat: " + textBoxTextChanged.Text;
+        }
+    }
+}
